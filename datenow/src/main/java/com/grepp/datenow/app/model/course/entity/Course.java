b1b5c071573942f0bcc028401ce1405c98383842
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2026-02-06 02:25:55 +0900

    refator : 연관관계중 바뀌어야할부분 전체 수정

--- datenow/src/main/java/com/grepp/datenow/app/model/course/entity/Course.java
+++ datenow/src/main/java/com/grepp/datenow/app/model/course/entity/Course.java
@@ -1,5 +1,6 @@
 package com.grepp.datenow.app.model.course.entity;
 
+import com.grepp.datenow.app.model.image.entity.Image;
 import com.grepp.datenow.app.model.member.entity.Member;
 import com.grepp.datenow.infra.entity.BaseEntity;
 import jakarta.persistence.CascadeType;
@@ -29,22 +30,12 @@ public class Course extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long coursesId;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id")
-    private Member id;
+
+
     private String title;
+
     @Lob
     private String description;
-    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Builder.Default
-    private List<CourseHashtag> courseHashtags = new ArrayList<>();
 
-    // 양방향 연관관계 주입 메서드
-    public void addCourseHashtag(Hashtag hashtag) {
-        CourseHashtag courseHashtag = new CourseHashtag(this, hashtag);
-        // Course 객체의 courseHashtags 리스트 컬렉션에 연결 테이블 객체를 추가
-        this.courseHashtags.add(courseHashtag);
-        // Hashtag 객체에도 연결 테이블 객체 추가해줌
-        hashtag.getCourseHashtags().add(courseHashtag);
-    }
+
 }
--- datenow/src/main/java/com/grepp/datenow/app/model/course/entity/EditorCourse.java
+++ datenow/src/main/java/com/grepp/datenow/app/model/course/entity/EditorCourse.java
@@ -1,17 +1,28 @@
 package com.grepp.datenow.app.model.course.entity;
 
+import com.grepp.datenow.app.model.image.entity.Image;
 import com.grepp.datenow.app.model.member.entity.Member;
 import com.grepp.datenow.infra.entity.BaseEntity;
 import jakarta.persistence.Entity;
 
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Setter
-//@ToString
 public class EditorCourse extends BaseEntity {
 
     @Id
@@ -21,9 +32,13 @@ public class EditorCourse extends BaseEntity {
     @JoinColumn(name = "id")
     private Member member;
     private String title;
+
     @Lob
     private String description;
-    //소프트 delete때문에
-    private Boolean activated =true;
+
+    @OneToMany(mappedBy = "editorCourseId", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Image> courseImages = new ArrayList<>();
+
 
 }
